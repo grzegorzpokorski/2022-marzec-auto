96ddd5106e6071719b24db9827e904616acb1323
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-03-17 23:53:34 +0100

    added paga.html and single.html (in progress)

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,30 @@
 import 'bootstrap';
+import { tns } from "./../../node_modules/tiny-slider/src/tiny-slider";
+
+const galleryInit = () => {
+	const sections = document.querySelectorAll('.gallery');
+
+	sections.forEach(section => {
+		const slider = section.querySelector('.hero-slider');
+		const controls = section.querySelector('.hero-slider__controls');
+
+		const heroSlider = tns({
+			container: slider,
+			controlsContainer: controls,
+			mode: 'carousel',
+			items: 1,
+			slideBy: 'page',
+			navPosition: 'bottom',
+			mouseDrag: true,
+			autoHeight: true,
+			autoplay: false,
+			autoplayHoverPause: true,
+			autoplayButtonOutput: false,
+			speed: 700,
+		});
+	});
+}
+
+if(document.querySelectorAll('.gallery')){
+	galleryInit();
+}
